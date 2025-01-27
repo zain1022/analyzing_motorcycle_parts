d9b7466dbafcc3f1934ce1e6be8300c3cb18270f
zain1022 <157541466+zain1022@users.noreply.github.com>
2025-01-27 15:14:49 -0600

    Create add_code.py

---
+++ data/add_code.py
@@ -0,0 +1,11 @@
+SELECT product_line,
+    CASE WHEN EXTRACT('month' from date) = 6 THEN 'June'
+        WHEN EXTRACT('month' from date) = 7 THEN 'July'
+        WHEN EXTRACT('month' from date) = 8 THEN 'August'
+    END as month,
+    warehouse,
+	SUM(total) - SUM(payment_fee) AS net_revenue
+FROM sales
+WHERE client_type = 'Wholesale'
+GROUP BY product_line, warehouse, month
+ORDER BY product_line, month, net_revenue DESC
